824f0a93275bdb636b87cbfdb9f5f079df4bac72
ntaranko <ntaranko@gmail.com>
2025-03-16 23:36:23 +0300

    refactor: add logging

--- src/main/java/ru/litres/pages/HomePage.java
+++ src/main/java/ru/litres/pages/HomePage.java
@@ -26,51 +26,65 @@ public class HomePage {
     }
 
     public HomePage openPage() {
+        logger.info("Open home page");
         driver.get(URL);
-        logger.info("Home page opened");
         return this;
     }
 
     public String getCopyRightTest() {
+        logger.info("Get copyright text");
         return driver.findElement(Locators.TEXT_COPYRIGHT).getText();
     }
 
     public void clickEnterButton() {
-        driver.findElement(Locators.BUTTON_ENTER).click();
+        logger.info("Wait for Enter button appears");
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement enterButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(Locators.BUTTON_ENTER));
+        logger.info("Click Enter button to login");
+        enterButton.click();
     }
 
     public void enterEmailOrLogin(String emailOrLogin) {
+        logger.info("Enter email or login");
         driver.findElement(Locators.FIELD_EMAIL_OR_LOGIN).sendKeys(emailOrLogin);
     }
 
     public void clickContinueButtonOnLoginForm() {
+        logger.info("Click continue button on Login form");
         driver.findElement(Locators.BUTTON_CONTINUE_LOGIN).click();
     }
 
     public String getErrorTextWhenLogin() {
+        logger.info("Get error text when login");
         return driver.findElement(Locators.TEXT_INPUT_ERROR).getText();
     }
 
     public String getEnterPasswordText() {
+        logger.info("Get Enter Password text");
         return driver.findElement(Locators.TEXT_ENTER_PASSWORD_FOR_USER).getText();
     }
 
     public void enterPassword(String password) {
+        logger.info("Enter password");
         driver.findElement(Locators.FIELD_PASSWORD).sendKeys(password);
     }
 
     public void clickEnterButtonOnLoginForm() {
+        logger.info("Click Enter button on login form");
         driver.findElement(Locators.BUTTON_ENTER_ON_LOGIN_FORM).click();
     }
 
     public CartPage clickCartButton() {
+        logger.info("Click Cart button to navigate to Cart page");
         driver.findElement(Locators.BUTTON_CART).click();
         return new CartPage();
     }
 
-    public WishListPage selectFavoriteBooksFromMenu() {
+    public WishListPage selectWishlistFromMenu() {
         Actions action = new Actions(driver);
         action.moveToElement(driver.findElement(Locators.BUTTON_MY_BOOKS)).perform();
+        logger.info("Select Liked Items menu");
         driver.findElement(Locators.MENU_LIKED_ITEM).click();
         return new WishListPage();
     }
@@ -85,57 +99,64 @@ public class HomePage {
     }
 
     public void enterSearchCriteria(String searchCriteria) {
+        logger.info("Wait for Search field appears");
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement fieldSearch = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(Locators.FIELD_SEARCH));
+        logger.info("Enter search criteria into field");
         fieldSearch.sendKeys(searchCriteria);
     }
 
     public void clickSearchButton() {
+        logger.info("Click Search button");
         driver.findElement(Locators.BUTTON_SEARCH).click();
     }
 
     public String getTextSearchResults() {
+        logger.info("Get Text search results");
         return driver.findElement(Locators.TEXT_SEARCH_RESULTS).getText();
     }
 
     public List<WebElement> getListOfBooks() {
+        logger.info("Get list of books");
         return driver.findElements(Locators.BOOK_ITEM);
     }
-
 
     public void openBook() {
         driver.findElement(Locators.BOOK_ITEM).click();
     }
 
-    public void addToFavourite(WebElement oneBook) {
+    public void addToWishlist(WebElement oneBook) {
+        logger.info("Add book from the list to wishlist");
         driver.findElement(Locators.BUTTON_BOOK_UNLIKED).click();
     }
 
-    public void removeFromFavourite(WebElement oneBook) {
+    public void removeFromWishlist(WebElement oneBook) {
+        logger.info("Remove liked book from wishlist");
         driver.findElement(Locators.BUTTON_BOOK_LIKED).click();
     }
 
     public void selectNewBooks() {
+        logger.info("Select New books area");
         driver.findElement(Locators.LINK_TEXT).click();
     }
 
-    public String getHeaderNewBooks() {
-        return driver.findElement(Locators.HEADER_NEW_BOOKS).getText();
-    }
-
     public BookPage selectBookFromList(List<WebElement> booksList, int i) {
+        logger.info("Select Book from list");
         booksList.get(i).click();
         Object[] windowHandles = driver.getWindowHandles().toArray();
+        logger.info("Switch to opened book page");
         driver.switchTo().window((String) windowHandles[1]);
         return new BookPage();
     }
 
     public boolean isButtonUnlikedExistForBook(WebElement oneBook) {
+        logger.info("Check if Unliked button exists for book");
         return ifElementExists(Locators.BUTTON_BOOK_UNLIKED);
     }
 
     public boolean isButtonLikedExistForBook(WebElement oneBook) {
+        logger.info("Check if Liked button exists for book");
         return ifElementExists(Locators.BUTTON_BOOK_LIKED);
     }
 
@@ -144,6 +165,7 @@ public class HomePage {
             driver.findElement(locator);
             return true;
         } catch (NoSuchElementException exception) {
+            logger.error(String.format("%s element doesn't exists", locator));
             return false;
         }
     }
