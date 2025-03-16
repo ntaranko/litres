813d3ec991eaf1660240aa1d0498be3d3805ad9b
ntaranko <ntaranko@gmail.com>
2025-03-16 23:31:45 +0300

    refactor add logging using logger

--- src/main/java/ru/litres/pages/BookPage.java
+++ src/main/java/ru/litres/pages/BookPage.java
@@ -1,5 +1,7 @@
 package ru.litres.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -7,50 +9,63 @@ import ru.litres.singleton.Singleton;
 
 public class BookPage {
     private WebDriver driver;
+    private static final Logger logger = LogManager.getLogger(BookPage.class);
 
     public BookPage() {
         this.driver = Singleton.getDriver();
     }
 
     public void clickAddToCartButton() {
+        logger.info("Click Add to cart button on book");
         driver.findElement(Locators.BUTTON_ADD_TO_CART).click();
     }
 
     public void closeModal() {
+        logger.info("Check if modal window exists");
         if (ifElementExists(Locators.DIALOG_MODAL_BUY_THREE_BOOKS)) {
+            logger.info("Close modal window when add book to cart");
             driver.findElement(Locators.BUTTON_CLOSE_MODAL).click();
         }
     }
 
     public String getNumberOfItemsFromBasket() {
+        logger.info("Check if Number of items icon exists");
         if (ifElementExists(Locators.ICON_NUMBER_OF_ITEMS_IN_CART)) {
+            logger.info("Get number of items from basket");
             return driver.findElement(Locators.ICON_NUMBER_OF_ITEMS_IN_CART).getText();
         } else {
+            logger.info("Number of items doesn't exist");
             return "";
         }
     }
 
     public boolean isAddToCartButtonExists() {
+        logger.info("Check if Add to cart button exists");
         return ifElementExists(Locators.BUTTON_ADD_TO_CART);
     }
 
     public boolean isGoToCartButtonExists() {
+        logger.info("Check if Go to cart button exists");
         return ifElementExists(Locators.BUTTON_GO_TO_CART);
     }
 
-    public void addToBookToWishlist() {
+    public void addBookToWishlist() {
+        logger.info("Add book to wishlist from book page");
         driver.findElement(Locators.BUTTON_ADD_TO_WISHLIST_FROM_BOOK_VIEW).click();
     }
 
     public boolean isIconFavoritesFilled() {
+        logger.info("Check if icon Favorite is filled");
         return ifElementExists(Locators.ICON_FAVORITES_FILLED);
     }
 
     public boolean isIconFavoritesNotFilled() {
+        logger.info("Chick if icon Favorite is not filled");
         return ifElementExists(Locators.ICON_FAVORITES_NOT_FILLED);
     }
 
     public CartPage clickCartButton() {
+        logger.info("Click Cart button to navigate to cart");
         driver.findElement(Locators.BUTTON_CART).click();
         return new CartPage();
     }
