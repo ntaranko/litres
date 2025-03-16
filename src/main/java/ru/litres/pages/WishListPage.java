ea0b5e7319f744729db209cb4ffb9374c5cf2de1
ntaranko <ntaranko@gmail.com>
2025-03-16 23:36:56 +0300

    refactor: add logging

--- src/main/java/ru/litres/pages/WishListPage.java
+++ src/main/java/ru/litres/pages/WishListPage.java
@@ -1,5 +1,7 @@
 package ru.litres.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -14,32 +16,48 @@ import java.util.List;
 
 public class WishListPage {
     private WebDriver driver;
+    private static final Logger logger = LogManager.getLogger(WishListPage.class);
 
     public WishListPage() {
         this.driver = Singleton.getDriver();
     }
 
     public String getNumberOfItemsOnWishlistTab() {
+        logger.info("Get number of items on wishlist tab");
         return driver.findElement(Locators.ICON_NUMBER_ITEMS_ON_WISHLIST_TAB).getText();
     }
 
     public String getNumberOfBooksOnWishlist() {
+        logger.info("Get number of books on wishlist");
         return driver.findElement(Locators.ICON_NUMBER_ITEMS_ON_WISHLIST_TAB).getText();
     }
 
     public String getNumberOfItemsInCart() {
+        logger.info("Get number of items in Cart");
         return driver.findElement(Locators.ICON_NUMBER_OF_ITEMS_IN_CART).getText();
     }
 
     public void removeBookFromWishList() {
-        driver.findElement(Locators.BUTTON_BOOK_MENU).click();
+        openMenuForBook();
+        logger.info("Wait for Remove form Wishlist menu appears");
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement menuItem = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(Locators.MENU_BOOK_REMOVE_FROM_WISHLIST));
+        logger.info("Click menu Remove from wishlist");
         menuItem.click();
     }
 
-    public void addBookToCartFromWishListQQQ(List<WebElement> booksList, int i) {
+    public void openMenuForBook() {
+        logger.info("Open menu for book");
+        driver.findElement(Locators.BUTTON_BOOK_MENU).click();
+    }
+
+    public void addBookToCartFromWishListQQQ(int index) {
+        List<WebElement> booksWishList = getListOfBooks(); // Always fetch latest list
+        booksWishList.get(index).click(); // Select the desired book
+    }
+
+   /* public void addBookToCartFromWishListQQQ(List<WebElement> booksList, int i) {
 
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement oneBook = booksList.get(i);
@@ -49,21 +67,25 @@ public class WishListPage {
         WebElement menuItem = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(Locators.MENU_BOOK_ADD_TO_CART));
         menuItem.click();
-    }
+    }*/
 
     public void addBookToCartFromWishList() {
-        driver.findElement(Locators.BUTTON_BOOK_MENU).click();
+        openMenuForBook();
+        logger.info("Wait for Add to Cart menu appears");
         Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement menuItem = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(Locators.MENU_BOOK_ADD_TO_CART));
+        logger.info("Click menu");
         menuItem.click();
     }
 
     public boolean isNumberOnTabExist() {
+        logger.info("Check if Icon with number on wishlist tab exists");
         return ifElementExists(Locators.ICON_NUMBER_ITEMS_ON_WISHLIST_TAB);
     }
 
     public List<WebElement> getListOfBooks() {
+        logger.info("Get list of books");
         return driver.findElements(Locators.BOOK_ITEM);
     }
 
@@ -72,6 +94,7 @@ public class WishListPage {
             driver.findElement(locator);
             return true;
         } catch (NoSuchElementException exception) {
+            logger.error("Element doesn't exist");
             return false;
         }
     }
