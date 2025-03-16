9e38f559683b73224a757c54c7b7faceaa03d984
ntaranko <ntaranko@gmail.com>
2025-03-16 23:32:54 +0300

    refactor: add Allure logging.

--- src/test/java/ru/litres/webtests/BookPageTest.java
+++ src/test/java/ru/litres/webtests/BookPageTest.java
@@ -1,81 +1,118 @@
 package ru.litres.webtests;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
-import ru.litres.singleton.Singleton;
 import ru.litres.pages.BookPage;
 import ru.litres.pages.HomePage;
+import ru.litres.singleton.Singleton;
 
 import java.util.List;
 
 public class BookPageTest {
     @Test
     @DisplayName("button text changed when add to cart")
-    public void test1() {
+    public void buttonTextTest() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        List<WebElement> booksList = homePage.getListOfBooks();
-        BookPage bookPage = homePage.selectBookFromList(booksList, 0);
-        bookPage.clickAddToCartButton();
-        bookPage.closeModal();
+        openHomePageAndSearch(homePage, searchCriteria);
+        List<WebElement> booksList = getListOfBooks(homePage);
+        BookPage bookPage = selectBookFromList(homePage, booksList, 0);
+        clickAddToCartButton(bookPage);
+        closeModal(bookPage);
 
-        Assertions.assertTrue(bookPage.isGoToCartButtonExists());
-        Assertions.assertFalse(bookPage.isAddToCartButtonExists());
+        Allure.step("Validating results", step -> {
+            Assertions.assertTrue(bookPage.isGoToCartButtonExists());
+            Assertions.assertFalse(bookPage.isAddToCartButtonExists());
+        });
     }
 
     @Test
     @DisplayName("number of items increased when add to cart")
-    public void test2() {
+    public void numberOfItemsTest() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        List<WebElement> booksList = homePage.getListOfBooks();
-        BookPage bookPage = homePage.selectBookFromList(booksList, 0);
-        bookPage.clickAddToCartButton();
-        bookPage.closeModal();
+        openHomePageAndSearch(homePage, searchCriteria);
+        List<WebElement> booksList = getListOfBooks(homePage);
+        BookPage bookPage = selectBookFromList(homePage, booksList, 0);
+        clickAddToCartButton(bookPage);
+        closeModal(bookPage);
 
-        Assertions.assertEquals("1", bookPage.getNumberOfItemsFromBasket());
+        Allure.step("Validating results", step -> {
+            Assertions.assertEquals("1", bookPage.getNumberOfItemsFromBasket());
+        });
     }
 
     @Test
     @DisplayName("add to wishlist from book page")
-    public void test3() {
+    public void AddToWishListTest() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        List<WebElement> booksList = homePage.getListOfBooks();
-        BookPage bookPage = homePage.selectBookFromList(booksList, 0);
-        bookPage.addToBookToWishlist();
+        openHomePageAndSearch(homePage, searchCriteria);
+        List<WebElement> booksList = getListOfBooks(homePage);
+        BookPage bookPage = selectBookFromList(homePage, booksList, 0);
+        addBookToWishlist(bookPage);
 
-        Assertions.assertTrue(bookPage.isIconFavoritesFilled());
-        Assertions.assertFalse(bookPage.isIconFavoritesNotFilled());
+        Allure.step("Validating results", step -> {
+            Assertions.assertTrue(bookPage.isIconFavoritesFilled());
+            Assertions.assertFalse(bookPage.isIconFavoritesNotFilled());
+        });
     }
 
     @Test
     @DisplayName("remove from wishlist on book page")
-    public void test4() {
+    public void removeFromWishlistTest() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        List<WebElement> booksList = homePage.getListOfBooks();
-        BookPage bookPage = homePage.selectBookFromList(booksList, 0);
-        bookPage.addToBookToWishlist();
-        bookPage.addToBookToWishlist();
+        openHomePageAndSearch(homePage, searchCriteria);
+        List<WebElement> booksList = getListOfBooks(homePage);
+        BookPage bookPage = selectBookFromList(homePage, booksList, 0);
+        addBookToWishlist(bookPage);
+        addBookToWishlist(bookPage);
 
-        Assertions.assertFalse(bookPage.isIconFavoritesFilled());
-        Assertions.assertTrue(bookPage.isIconFavoritesNotFilled());
+        Allure.step("Validating results", step -> {
+            Assertions.assertFalse(bookPage.isIconFavoritesFilled());
+            Assertions.assertTrue(bookPage.isIconFavoritesNotFilled());
+        });
     }
 
     @AfterEach
     public void afterEach() {
         Singleton.quit();
+    }
+
+    @Step("precondition: open home page and search")
+    void openHomePageAndSearch(HomePage homePage, String searchCriteria) {
+        homePage.openPage();
+        homePage.search(searchCriteria);
+    }
+
+    @Step("Get books list")
+    List<WebElement> getListOfBooks(HomePage homePage) {
+        return homePage.getListOfBooks();
+    }
+
+    @Step("Select book from list to open book page")
+    BookPage selectBookFromList(HomePage homePage, List<WebElement> booksList, int i) {
+        return homePage.selectBookFromList(booksList, i);
+    }
+
+    @Step("Click Add to Cart button")
+    void clickAddToCartButton(BookPage bookPage) {
+        bookPage.clickAddToCartButton();
+    }
+
+    @Step("Close modal")
+    void closeModal(BookPage bookPage) {
+        bookPage.closeModal();
+    }
+
+    @Step("Add book to wishlist")
+    void addBookToWishlist(BookPage bookPage) {
+        bookPage.addBookToWishlist();
     }
 }
