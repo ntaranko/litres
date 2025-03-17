4cea05d8800f487213f86a15b8c11cd3ab0120c1
ntaranko <ntaranko@gmail.com>
2025-03-17 05:14:51 +0300

    refactor Test

--- src/test/java/ru/litres/apitests/ApiCartTest.java
+++ src/test/java/ru/litres/apitests/ApiCartTest.java
@@ -1,6 +1,9 @@
 package ru.litres.apitests;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
@@ -15,23 +18,30 @@ import static ru.litres.api.ApiUtils.isBookInCard;
 public class ApiCartTest {
 
     @Test
-    @DisplayName("Add book to cart - verify there is no error")
+    @DisplayName("Add book to cart - verify the book is on the cart")
     public void testAddBookToCart() {
         RestAssured.baseURI = "https://api.litres.ru/";
         String bookIdToAdd = "63920696";
         String body = "{\"art_ids\":[" + bookIdToAdd + "]}";
+        Header header = new Header("content-type", "application/json");
 
-        Response response = given()
-                .header("content-type", "application/json")
-                .body(body)
-                .put("foundation/api/cart/arts/add")
-                .then()
-                .log().body()
-                .extract().response();
-
+        Response response = getResponse(body, header, "foundation/api/cart/arts/add");
         JsonPath jsonPath = response.jsonPath();
         List<Integer> booksInCartList = jsonPath.getList("payload.data.added_art_ids");
 
-        Assertions.assertEquals(true, isBookInCard(booksInCartList, bookIdToAdd));
+        Allure.step("Validating results", step -> {
+            Assertions.assertEquals(true, isBookInCard(booksInCartList, bookIdToAdd));
+        });
     }
+
+    @Step("Get response: send get request with search")
+    Response getResponse(String body, Header header, String url) {
+        return given()
+                .header(header)
+                .body(body)
+                .put("foundation/api/cart/arts/add")
+                .then()
+                .extract().response();
+    }
+
 }
