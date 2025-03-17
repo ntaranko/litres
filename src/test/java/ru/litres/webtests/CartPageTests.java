93846909388c0e4914193aa97ad9116871f88a43
ntaranko <ntaranko@gmail.com>
2025-03-17 03:28:39 +0300

    refactor: add Allure logging and tests

--- src/test/java/ru/litres/webtests/CartPageTests.java
+++ src/test/java/ru/litres/webtests/CartPageTests.java
@@ -1,5 +1,6 @@
 package ru.litres.webtests;
-
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,44 +10,26 @@ import ru.litres.pages.CartPage;
 import ru.litres.pages.HomePage;
 import ru.litres.pages.WishListPage;
 import ru.litres.singleton.Singleton;
-
 import java.util.List;
 
 public class CartPageTests {
-
-    //TBD or remove
-    @Test
-    @DisplayName("validate number of books in cart")
-    public void test1() {
-        String searchCriteria = "рабле";
-        HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        homePage.addToFavourite(homePage.getListOfBooks().get(0));
-        homePage.addToFavourite(homePage.getListOfBooks().get(1));
-        homePage.addToFavourite(homePage.getListOfBooks().get(3));
-
-        WishListPage wishListPage = homePage.selectFavoriteBooksFromMenu();
-        List<WebElement> booksWishList = wishListPage.getListOfBooks();
-        wishListPage.addBookToCartFromWishListQQQ(booksWishList, 2);
-        booksWishList = wishListPage.getListOfBooks();
-        wishListPage.addBookToCartFromWishListQQQ(booksWishList, 0);
-        CartPage cartPage = homePage.clickCartButton();
-        Assertions.assertEquals(2, cartPage.getBooksList().size());
-    }
 
     @Test
     @DisplayName("validate number of books on cart icon")
     public void testNumberOfBooksOnIcon() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        homePage.addToFavourite(homePage.getListOfBooks().get(0));
-        WishListPage wishListPage = homePage.selectFavoriteBooksFromMenu();
-        wishListPage.addBookToCartFromWishList();
-        CartPage cartPage = homePage.clickCartButton();
-        Assertions.assertEquals("1", cartPage.getNumberOnCartIcon());
+        openHomePageAndSearch(homePage, searchCriteria);
+        addBookToWishlist(homePage, 0);
+        addBookToWishlist(homePage, 1);
+        addBookToWishlist(homePage, 2);
+        WishListPage wishListPage = openWishListPageFromMenu(homePage);
+        addBookToCartFromWishList(wishListPage, 1);
+        CartPage cartPage = clickCartButton(homePage);
+
+        Allure.step("Validating results", step -> {
+            Assertions.assertEquals("1", cartPage.getNumberOnCartIcon());
+        });
     }
 
     @Test
@@ -54,42 +37,110 @@ public class CartPageTests {
     public void testNumberOfBooksOnCardList() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        homePage.addToFavourite(homePage.getListOfBooks().get(0));
-        WishListPage wishListPage = homePage.selectFavoriteBooksFromMenu();
-        wishListPage.addBookToCartFromWishList();
-        CartPage cartPage = homePage.clickCartButton();
+        openHomePageAndSearch(homePage, searchCriteria);
+        addBookToWishlist(homePage, 0);
+        addBookToWishlist(homePage, 1);
+        addBookToWishlist(homePage, 2);
+        WishListPage wishListPage = openWishListPageFromMenu(homePage);
+        addBookToCartFromWishList(wishListPage, 2);
+        CartPage cartPage = clickCartButton(homePage);
+
         Assertions.assertEquals(1, cartPage.getBooksList().size());
     }
 
     @Test
-    @DisplayName("remove from cart")
-    public void testRemoveBookFromCart() {
+    @DisplayName("remove from cart - validate text for empty card")
+    public void removeBookFromCartTextTest() {
         String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
-        homePage.search(searchCriteria);
-        homePage.addToFavourite(homePage.getListOfBooks().get(0));
-        WishListPage wishListPage = homePage.selectFavoriteBooksFromMenu();
-        wishListPage.addBookToCartFromWishList();
-        CartPage cartPage = homePage.clickCartButton();
-        cartPage.removeBookFromCart();
-        cartPage.closeModal();
-        cartPage.getTextEmptyCart();
-        Assertions.assertEquals("Корзина пуста", cartPage.getTextEmptyCart());
+        openHomePageAndSearch(homePage, searchCriteria);
+        addBookToWishlist(homePage, 0);
+        addBookToWishlist(homePage, 1);
+        addBookToWishlist(homePage, 2);
+        WishListPage wishListPage = openWishListPageFromMenu(homePage);
+        addBookToCartFromWishList(wishListPage, 2);
+        CartPage cartPage = clickCartButton(homePage);
+        removeBookFromCart(cartPage);
+        closeModal(cartPage);
+
+        Allure.step("Validating results", step -> {
+            Assertions.assertEquals("Корзина пуста", getTextEmptyCart(cartPage));
+        });
     }
 
     @Test
-    @DisplayName("increases when remove from cart")
-    public void test5() {
+    @DisplayName("remove from cart - validate icon number is not shown for empty cart")
+    public void removeBookFromCartNumberTest() {
+        String searchCriteria = "рабле";
         HomePage homePage = new HomePage();
-        homePage.openPage();
+        openHomePageAndSearch(homePage, searchCriteria);
+        addBookToWishlist(homePage, 0);
+        addBookToWishlist(homePage, 1);
+        addBookToWishlist(homePage, 2);
+        WishListPage wishListPage = openWishListPageFromMenu(homePage);
+        addBookToCartFromWishList(wishListPage, 2);
+        CartPage cartPage = clickCartButton(homePage);
+        removeBookFromCart(cartPage);
+        closeModal(cartPage);
 
+        Allure.step("Validating results", step -> {
+            Assertions.assertEquals(false, isNumberOnCartIconNotPresent(cartPage));
+        });
     }
 
     @AfterEach
     public void afterEach() {
         Singleton.quit();
+    }
+
+    @Step("precondition: open home page and search")
+    void openHomePageAndSearch(HomePage homePage, String searchCriteria) {
+        homePage.openPage();
+        homePage.search(searchCriteria);
+    }
+
+    @Step("Precondition: add book to wishlist")
+    void addBookToWishlist(HomePage homePage, int i) {
+        homePage.addToWishlist(homePage.getListOfBooks().get(i));
+    }
+
+    @Step("open wishlist")
+    WishListPage openWishListPageFromMenu(HomePage homePage) {
+        return homePage.selectWishlistFromMenu();
+    }
+
+    @Step("add book to cart")
+    void addBookToCartFromWishList(WishListPage wishListPage, int bookIndex) {
+        wishListPage.addBookToCartFromWishList(bookIndex);
+    }
+
+    @Step("Open Cart page")
+    CartPage clickCartButton(HomePage homePage) {
+        return homePage.clickCartButton();
+    }
+
+    @Step("Get books list")
+    List<WebElement> getListOfBooks(WishListPage wishListPage) {
+        return wishListPage.getListOfBooks();
+    }
+
+    @Step("Remove book from cart")
+    void removeBookFromCart(CartPage cartPage) {
+        cartPage.removeBookFromCart();
+    }
+
+    @Step("Close modal window when removing book from cart")
+    void closeModal(CartPage cartPage) {
+        cartPage.closeModal();
+    }
+
+    @Step("Get text for empty cart")
+    String getTextEmptyCart(CartPage cartPage) {
+        return cartPage.getTextEmptyCart();
+    }
+
+    @Step("Check if ivon with number exists for cart")
+    Boolean isNumberOnCartIconNotPresent(CartPage cartPage) {
+        return cartPage.isNumberOnCartIconNotPresent();
     }
 }
