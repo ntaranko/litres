6850f34d9c4a222a37bf5b9cb8fee4a870af8132
ntaranko <ntaranko@gmail.com>
2025-03-17 03:28:20 +0300

    refactor: add logging and methods

--- src/main/java/ru/litres/pages/CartPage.java
+++ src/main/java/ru/litres/pages/CartPage.java
@@ -1,5 +1,7 @@
 package ru.litres.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -10,27 +12,44 @@ import java.util.List;
 
 public class CartPage {
     private WebDriver driver;
+    private static final Logger logger = LogManager.getLogger(CartPage.class);
 
     public CartPage() {
         this.driver = Singleton.getDriver();
     }
 
     public List<WebElement> getBooksList() {
+        logger.info("Get books list");
         return driver.findElements(Locators.BOOK_IN_CART);
     }
 
-    public void removeBookFromCart(){
-       driver.findElement(Locators.BUTTON_REMOVE_BOOK_FROM_CART).click();
+    public void removeBookFromCart() {
+        logger.info("Remove book from cart");
+        driver.findElement(Locators.BUTTON_REMOVE_BOOK_FROM_CART).click();
     }
 
     public void closeModal() {
+        logger.info("Check if modal window exists");
         if (ifElementExists(Locators.MODAL_CONFIRM_REMOVE_BOOK)) {
+            logger.info("Close the modal");
             driver.findElement(Locators.BUTTON_CONFIRM_REMOVE_BOOK).click();
         }
     }
 
     public String getNumberOnCartIcon() {
+        logger.info("Get Number on cart icon");
         return driver.findElement(Locators.ICON_NUMBER_OF_ITEMS_IN_CART).getText();
+    }
+
+    public String getTextEmptyCart() {
+        logger.info("Get text on empty cart");
+        return driver.findElement(Locators.TEXT_EMPTY_CART).getText();
+    }
+
+    public boolean isNumberOnCartIconNotPresent(){
+        logger.info("Check if ivon with number exists for cart");
+        return ifElementExists(Locators.ICON_NUMBER_OF_ITEMS_IN_CART);
+
     }
 
     public boolean ifElementExists(By locator) {
@@ -38,11 +57,8 @@ public class CartPage {
             driver.findElement(locator);
             return true;
         } catch (NoSuchElementException exception) {
+            logger.error(String.format("%s element doesn't exists", locator));
             return false;
         }
-    }
-
-    public String getTextEmptyCart() {
-        return driver.findElement(Locators.TEXT_EMPTY_CART).getText();
     }
 }
